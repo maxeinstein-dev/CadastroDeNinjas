5f377484e8ae04e080c41b259270fbf137f1f996
Maxsuel Einstein <maxsueleinstein.dev@gmail.com>
2026-03-26 11:38:07 -0300

    Adiciona classe Ninja

--- src/main/java/br/com/maxsueleinstein/cadastrodeninjas/model/Ninja.java
+++ src/main/java/br/com/maxsueleinstein/cadastrodeninjas/model/Ninja.java
@@ -11,4 +11,40 @@ public class Ninja {
     private String nome;
     private int idade;
     private String email;
+
+    public Ninja() {}
+
+    public Ninja(String nome, int idade, String email) {
+        this.nome = nome;
+        this.idade = idade;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
