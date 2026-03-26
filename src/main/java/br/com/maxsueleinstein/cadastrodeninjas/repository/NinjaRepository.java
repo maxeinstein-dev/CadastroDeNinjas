e113799bdad59fade9ae27e69425ab0aabd3a318
Maxsuel Einstein <maxsueleinstein.dev@gmail.com>
2026-03-26 13:16:41 -0300

    Adiciona Ninja repository

--- src/main/java/br/com/maxsueleinstein/cadastrodeninjas/repository/NinjaRepository.java
+++ src/main/java/br/com/maxsueleinstein/cadastrodeninjas/repository/NinjaRepository.java
@@ -1,4 +1,9 @@
 package br.com.maxsueleinstein.cadastrodeninjas.repository;
 
-public class NinjaRepository {
+import br.com.maxsueleinstein.cadastrodeninjas.model.Ninja;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NinjaRepository extends JpaRepository<Ninja, Long> {
 }
